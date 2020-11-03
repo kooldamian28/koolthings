ca2020e641d4b12e48004b1bb54867678f4005c6
Damian Jude Mascarenhas <67428121+kooldamian28@users.noreply.github.com>
2020-11-03 08:25:08 +0400

    Add files via upload

---
+++ kool/WAP to accept a number from the user and display whether it is an even number or an odd number.py
@@ -0,0 +1,8 @@
+#User input
+num = float(input("Enter your number: "))
+
+# Program operation & computer output
+if (num % 2) == 0:
+    print(str(num) + " is even.")
+else:
+    print(str(num) + " is odd.")
